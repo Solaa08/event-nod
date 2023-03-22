a5af209a0c2fc9d1064537ffb1e8d8234793ad4e
arcanom <60106527+arcanom@users.noreply.github.com>
2023-03-22 15:40:49 +0100

    feat(form-event): Tailwind Eslint config

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {} from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const  App =():  JSX.Element =>  {
 
   return (
     <div className="App">
