8e578a1b23b367a9acf7dad29bc7617cfda0707d
arcanom <60106527+arcanom@users.noreply.github.com>
2023-03-23 14:39:04 +0100

    feat(form event): type of objet sended

--- front/src/containers/FormEvent.tsx
+++ front/src/containers/FormEvent.tsx
@@ -4,38 +4,44 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from '../config/firebase'
 
 export const FormEvent = (): JSX.Element => {
-  const eventCollection = collection(db, "events")
 
-  const addEvent = async (object : any) => {
-    try {
-      await addDoc(eventCollection, {
-        title: object.title,
-        description: object.description,
-        date: object.date
-      })
-    } catch (error) {
-      console.error(error)
+    interface EventType {
+        title: string
+        description: string
+        date : Date
     }
-  }
-    
-  useEffect(() => {
-    // addEvent()
-  }, [])
-  return (
-    <div className='w-full h-full flex justify-center items-center '>
+    const eventCollection = collection(db, "events")
 
-      <div className='w-1/3 h-1/3'>
-        <Form onSubmit={(object: any) => { addEvent(object) }}>
-          <Label> Titre </Label>
-          <Field type="text" name="title" /> 
-          <Label> Description </Label>
-          <Textarea name="description"></Textarea> 
-          <Label> Date </Label>
-          <Field type="date" name="date"></Field>
-          <Submit>creer</Submit>
-        </Form>
+    const addEvent = async (object : EventType) => {
+      try {
+        await addDoc(eventCollection, {
+          title: object.title,
+          description: object.description,
+          date: object.date
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    
+    useEffect(() => {
+    // addEvent()
+    }, [])
+    return (
+      <div className='w-full h-full flex justify-center items-center '>
+
+        <div className='w-1/3 h-1/3'>
+          <Form onSubmit={(object: any) => { addEvent(object) }}>
+            <Label> Titre </Label>
+            <Field type="text" name="title" /> 
+            <Label> Description </Label>
+            <Textarea name="description"></Textarea> 
+            <Label> Date </Label>
+            <Field type="date" name="date"></Field>
+            <Submit>creer</Submit>
+          </Form>
+        </div>
       </div>
-    </div>
-  )
+    )
 }
 
