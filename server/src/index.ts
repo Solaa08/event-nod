bac9fc03a580e5f5ac4e8a8fe8e7e4c14424d11a
Loick T <58403379+Solaa08@users.noreply.github.com>
2023-03-23 09:17:44 +0100

    feat(backend-init): express app init + Typescript config

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+require('dotenv').config()
+import express from "express"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = 5000
+
+app.listen(PORT)
+console.log(`listening on port ${PORT}`)
+// descriptions
