6dd45577483e7d0bc7581ccf89c95ada2952c1dc
Loick T <58403379+Solaa08@users.noreply.github.com>
2023-03-27 09:30:46 +0200

    fix(cards): firebase timestamp to date format

--- front/src/containers/Events.tsx
+++ front/src/containers/Events.tsx
@@ -3,6 +3,7 @@ import { db } from '../config/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import { useState } from 'react';
 import Event from './Event';
+import moment from 'moment';
 
 
 const Events = (): JSX.Element => {
@@ -31,9 +32,13 @@ const Events = (): JSX.Element => {
   return (
     <div>
       <div>
-        {events?.map((event: { id: React.Key; title: string; description: string; date: number; }) => (
+        {events?.map((event: { id: React.Key; title: string; description: string; date: Date | any; }) => (
           <li key={event.id}>
-            <Event title={event.title} description={event.description} date={event.date}/>
+            <Event 
+              title={event.title} 
+              description={event.description} 
+              date={moment(event.date.toDate()).format('MMMM Do YYYY, h:mm:ss a')}
+            />
           </li>
         ))}
       </div>
